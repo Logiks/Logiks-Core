b24af5e0e9d0035f34b6c3c75d592e29475a1b7e
Bismay <bismay@openlogiks.org>
2015-11-05 01:22:35 +0530

    .....

--- tests/test_generic.php
+++ tests/test_generic.php
@@ -3,14 +3,14 @@ class test_generic extends PHPUnit_Framework_TestCase
 {
 	public function testEmpty()
     {
-        $this->assertEquals(1, 1);
+        $this->assertEquals(1, 0);
     }
     
 	/**
      * @depends testEmpty
      */
     public function testEquality() {
-        $this->assertTrue(false);
+        $this->assertTrue(true);
     }
 }
 ?>
