40255ac1c54536d93157cda0d1766f28b626d6d6
Bismay <bismay4u@gmail.com>
2018-01-13 05:01:10 +0530

    Removed the bin directory
    Removed the bin/scripts, moved them into Logiks-Installer-Bash so that they can be worked upon independently.

--- bin/update-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd ..;
-cp -R config/jsonConfig ../
-find . -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull origin master" >tmp/update.log \;
-cp -Rv ../jsonConfig/* config/jsonConfig/
-rm -R ../jsonConfig
