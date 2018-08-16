4e66690137bcad12254c9d022d7c2d55f6dfa40b
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2018-08-16 23:57:03 +0530

    Updates to Logiks-PHPUnit Test System
    Added a new test case.

---
+++ tests/services/test_services_alive.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * For testing logiks core helper : alive
+ * 
+ * @author	Bismay <bismay4u@gmail.com>
+ * @date 	2018-08-16
+ */
+class test_helpers_alive extends LogiksTestCase {
+	
+	public function setUp() {
+		parent::setUp();
+		//loadHelpers("browser");
+	}
+	
+	public function test_checkBrowser() {
+	    $ans = $this->http_get("http://localhost/services/alive?site=cms");
+		$result = $ans[0];
+	    
+	    $time=date("r");
+	    
+	    $this->assertEquals("data: SERVER {$time}",trim($result));
+	}
+}
+?>
+
