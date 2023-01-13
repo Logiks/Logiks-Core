abdb235a7cef3053da8952278a4fc9132e15d8e8
Root User <admin@demo.com>
2023-01-13 18:55:17 +0530

    Updates to RoleModel

--- bin/checkUpdates.sh
+++ bin/checkUpdates.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-git pull origin master
+git pull origin $(git rev-parse --abbrev-ref HEAD)
 echo ""
 
 if [ -d "pluginsDev" ] 
 then
     cd pluginsDev/modules/
-    find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin master && echo)' \;
+    find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin $(git rev-parse --abbrev-ref HEAD) && echo)' \;
     cd ../
     
     cd vendors/
-    find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin master && echo)' \;
+    find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin $(git rev-parse --abbrev-ref HEAD) && echo)' \;
 
     cd ../../
 fi
 
 cd pluginsDev/modules/
-find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin master && echo)' \;
+find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin $(git rev-parse --abbrev-ref HEAD) && echo)' \;
 cd ../
 
 cd vendors/
-find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin master && echo)' \;
+find . -maxdepth 1 -mindepth 1 -type d -exec sh -c '(echo {} && cd {} && pwd && git pull origin $(git rev-parse --abbrev-ref HEAD) && echo)' \;
 cd ../../
 
 echo "Updates Checking Complete";
